27d1b3b52fcc2977e31a51a366dd68484f5bb2a7
Bari Sopa <barisopa1@hotmail.com>
2025-06-11 16:34:19 +0200

    can create user with given struct

--- api.go
+++ api.go
@@ -1,6 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(w)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w ,r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -12,12 +40,32 @@ func newAPIServer(listenAddr string) *APIServer {
 	}
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	http.ListenAndServe(s.listenAddr, router)
+
+	log.Println("JSON API server running on port: ", s.listenAddr)
+}
+
 func (s *APIServer) handleAccount(w http.ResponseWriter,r *http.Request) error {
-	return nil
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("method not allowed %s", r.Method)
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter,r *http.Request) error {
-	return nil
+	account := NewAccount("Bari", "Sopa")
+	return WriteJSON(w, http.StatusOK, account)
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter,r *http.Request) error {
--- main.go
+++ main.go
@@ -1,4 +1,10 @@
 package main
 
+import "fmt"
+
+
 func main() {
+	server := newAPIServer(":8080")
+	server.Run()
+	fmt.Println("Server running on ", server)
 }
